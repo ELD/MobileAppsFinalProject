2631ffbfc363b7591c539d1e6d4c226eea492ce0
Stephen Hamrick <Stephen Hamrick>
2016-05-06 20:31:58 -0600

    ADded settings page

--- TwovieTimes/app/src/main/java/com/fandb/twovietimes/Settings.java
+++ TwovieTimes/app/src/main/java/com/fandb/twovietimes/Settings.java
@@ -4,10 +4,13 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.UUID;
+
 public class Settings extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
         return new SettingsFragment();
     }
+
 }
--- TwovieTimes/app/src/main/java/com/fandb/twovietimes/SettingsFragment.java
+++ TwovieTimes/app/src/main/java/com/fandb/twovietimes/SettingsFragment.java
@@ -1,9 +1,51 @@
 package com.fandb.twovietimes;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.NumberFormat;
 
 /**
  * Created by eric on 5/6/16.
  */
 public class SettingsFragment extends Fragment {
+    EditText mRadius;
+    EditText mZip;
+
+    Button mApply;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstance){
+         super.onCreate(savedInstance);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_settings, container, false);
+
+        mRadius = (EditText) v.findViewById(R.id.radius);
+        mZip = (EditText) v.findViewById(R.id.zip);
+
+        mApply = (Button) v.findViewById(R.id.apply);
+        mApply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mRadius.getText().length() > 0){
+                    APIHandler.mRadius = Integer.parseInt(mRadius.getText().toString());
+                }
+                if(mZip.getText().length() == 5){
+                    APIHandler.mZip = mRadius.getText().toString();
+                }
+            };
+        });
+
+        return v;
+    }
 }
